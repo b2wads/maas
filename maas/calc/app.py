ad80c12cf6d6a442bc38dde83474640a2919f2a2
Dalton Barreto <daltonmatos@gmail.com>
2019-05-29 10:37:25 -0300

    Adicionando validação no endpoint `/eval`
    Fazemos duas validações.
    
     - Uma para verificar se a expressão foi parseada com sucesso
     - Outra para detectar algunm erro na chamada aos demais micro-serviços

--- maas/calc/app.py
+++ maas/calc/app.py
@@ -11,5 +11,29 @@ async def calc(request: web.Request):
     expr = (await request.json())["expr"]
     parser = Tree()
     parser.parse(expr)
-    result = await parser.root.eval()
-    return web.json_response({"result": result})
+
+    try:
+        parser.evaluate()
+    except Exception as e:
+        return web.json_response(
+            {
+                "result": None,
+                "error": {"exc": str(e), "reason": "Invalid Expression"},
+            },
+            status=400,
+        )
+
+    try:
+        result = await parser.root.eval()
+        return web.json_response({"result": result})
+    except Exception as e:
+        return web.json_response(
+            {
+                "result": None,
+                "error": {
+                    "exc": str(e),
+                    "reason": "Error evaluating expression",
+                },
+            },
+            status=500,
+        )
